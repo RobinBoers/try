412d8b0167548cb74237c5cab9962cc0afd6fcb6
Robin <robindev2019@outlook.com>
2025-12-27 18:49:57 +0100

    Bump version & rerelease

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Try.MixProject do
   def project do
     [
       app: :try,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
